cc197712032687017458a65d18b93f71ee7e0f6c
Morten Jelle <1551620+appelsinvandi@users.noreply.github.com>
2021-12-16 01:09:14 +0100

    add: fileDimensionsChecker

---
+++ script/fileDimensionsChecker.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+const sizeOf = require('image-size')
+
+const rootFolder = path.resolve(__dirname, '../fileDimensionsCheckerInput')
+
+function checkPngsDeep(folder) {
+  const files = fs.readdirSync(folder)
+  files.forEach((file) => {
+    const filePath = path.resolve(folder, file)
+    if (fs.statSync(filePath).isDirectory()) {
+      checkPngsDeep(filePath)
+    } else if (path.extname(file) === '.png') {
+      const dimensions = sizeOf(filePath)
+      if (dimensions.width !== 2070 || dimensions.height !== 2070) {
+        console.log(`${file} has wrong dimensions: ${dimensions.width}x${dimensions.height}`)
+      }
+    }
+  })
+}
+
+checkPngsDeep(rootFolder)
