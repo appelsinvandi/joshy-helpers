e3b0c71b7383c83e1f4c7a5affe1d0b5a5a51fec
Morten Jelle <1551620+appelsinvandi@users.noreply.github.com>
2021-12-17 15:11:42 +0100

    feat(fmt-addr): list length warn

--- scripts/formatAddresses.js
+++ scripts/formatAddresses.js
@@ -4,4 +4,10 @@ const path = require('path')
 const inputFile = path.resolve(__dirname, '..', 'inputs', 'formatAddresses.txt')
 const inputFileContents = fs.readFileSync(inputFile, 'utf8')
 
-console.log('["' + inputFileContents.replace(/\s+/gi, '\n').split('\n').filter(Boolean).join('","') + '"]')
+const addresses = inputFileContents.replace(/\s+/gi, '\n').split('\n').filter(Boolean)
+
+console.log('["' + addresses.join('","') + '"]')
+if (addresses.length > 250) {
+  console.warn('= WARNING =')
+  console.warn('Too many elements in input! Got ' + addresses.length + ' elements.')
+}
